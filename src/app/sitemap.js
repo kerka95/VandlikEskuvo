0f9bbcfc71bff88f8b9cb57bb3e671a2eff9f573
Vandlik Eskuvo <info@vandik.hu>
2026-05-31 13:37:31 +0200

    static build

---
+++ src/app/sitemap.js
@@ -0,0 +1,39 @@
+export const dynamic = "force-static";
+
+export default function sitemap() {
+  // TODO: Cseréld ki a valós domain névre, ha megvan
+  const baseUrl = 'https://vandliktestverek.hu'; 
+
+  return [
+    {
+      url: `${baseUrl}`,
+      lastModified: new Date(),
+      changeFrequency: 'yearly',
+      priority: 1,
+    },
+    {
+      url: `${baseUrl}/about`,
+      lastModified: new Date(),
+      changeFrequency: 'monthly',
+      priority: 0.8,
+    },
+    {
+      url: `${baseUrl}/services`,
+      lastModified: new Date(),
+      changeFrequency: 'monthly',
+      priority: 0.8,
+    },
+    {
+      url: `${baseUrl}/gallery`,
+      lastModified: new Date(),
+      changeFrequency: 'weekly',
+      priority: 0.9,
+    },
+    {
+      url: `${baseUrl}/contact`,
+      lastModified: new Date(),
+      changeFrequency: 'yearly',
+      priority: 0.7,
+    },
+  ];
+}
